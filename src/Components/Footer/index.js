bc99b225d7b6cdfb29ff15d270ff46437c038070
Cindy <zxx.njc@gmail.com>
2019-08-04 16:23:13 +0800

    update footer text

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -15,7 +15,7 @@ export default function Footer(props) {
     return (
         <section className={classes.root} style={{backgroundColor: props.color}}>                
             <center>
-                <h5 style={{ fontSize: "18px"}}>Proudly designed and developed by SmuAI.<br/>For enquiries/feedback on the website, kindly contact <a href="https://t.me/slavetojavascript" style={{textDecoration: "none", color: "#1b2a52"}}>@SlaveToJavascript</a> on Telegram.</h5>
+                <h5 style={{ fontSize: "18px"}}>Proudly designed and developed by SmuAI.<br/>Kindly contact <a href="https://t.me/slavetojavascript" style={{textDecoration: "none", color: "#1b2a52"}}>@SlaveToJavascript</a> on Telegram for enquiries/feedback on the website.</h5>
             </center>
         </section>
     )
