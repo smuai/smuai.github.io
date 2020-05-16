f1629bf3b391d6435b5018a549ae84fd157c422c
Cindy <zxx.njc@gmail.com>
2020-05-16 21:01:20 +0800

    ESLint fixes

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,20 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Landing from "./Landing";
 import About from "./About";
 import Do from "./Do"
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-    root: {
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundAttachment: "fixed",
-    }
-})
 
 export default function Home() {
-    const classes = useStyles();
-
     return (
         <div>
             <Landing/>
